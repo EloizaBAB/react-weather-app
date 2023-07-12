e93517f3165d430c637b4c6a22e6707d3b159f26
EloizaBAB <122025679+EloizaBAB@users.noreply.github.com>
2023-07-12 18:48:07 +0100

    used the hook UseState to display the temperature in celsius and in fahrenheit,crated functions and added event listeners

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(properties) {
-  return (
-    <div>
-      <span>{Math.round(properties.info.temperature)}</span>
-      <span>ºC</span>
-    </div>
-  );
+  const [unit, setunit] = useState("celsius");
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setunit("celsius");
+  }
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setunit("Fahrenheit");
+  }
+  if (unit === "celsius") {
+    return (
+      <div>
+        <span>{Math.round(properties.celsius)}</span>
+        <span>°C | </span>
+        <span>
+          <a href="/" onClick={convertToFahrenheit}>
+            F°
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (properties.celsius * 9) / 5 + 32;
+    return (
+      <div>
+        <span>{Math.round(fahrenheit)}</span>
+        <span>
+          {" "}
+          <a href="/" onClick={convertToCelsius}>
+            °C{" "}
+          </a>
+          |{" "}
+        </span>
+        <span>F°</span>
+      </div>
+    );
+  }
 }
